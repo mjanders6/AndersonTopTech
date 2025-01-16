bb6e3cf5d1cf34c0ee71d53bd315613f67791df9
mjanders6 <mjanders6@gmail.com>
2025-01-15 21:04:04 -0800

    color schemes

--- js/NavBar.js
+++ js/NavBar.js
@@ -16,8 +16,9 @@ customElements.define('custom-nav', class extends HTMLElement {
   // Render the navigation bar
   render() {
     this.innerHTML = `
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
           <div class="container-fluid">
+            <a class="navbar-brand">AndersonTopTech</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               ${this.links
           .map(
