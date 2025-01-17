c66a30aed98c80180d75781da8dc30b182b7b9eb
mjanders6 <mjanders6@gmail.com>
2025-01-16 19:37:52 -0800

    phone view

--- js/NavBar.js
+++ js/NavBar.js
@@ -16,17 +16,22 @@ customElements.define('custom-nav', class extends HTMLElement {
   // Render the navigation bar
   render() {
     this.innerHTML = `
-        <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
           <div class="container-fluid">
-            <a class="navbar-brand">AndersonTopTech</a>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              ${this.links
-          .map(
-            link =>
-              `<li class="nav-item"><a class="nav-link active" aria-current="page"  href="${link.href}" target="_self">${link.name}</a></li>`
-          )
-          .join('')}
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">AndersonTopTech</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                ${this.links
+        .map(
+          link =>
+            `<li class="nav-item"><a class="nav-link active" aria-current="page"  href="${link.href}" target="_self">${link.name}</a></li>`
+        )
+        .join('')}
+              </ul>
+            </div>
           </div>
         </nav>
       `;
